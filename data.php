b8917677136d79126237d38070bbf981ac58c234
LUKAS FERREIRA FAZA <25254060@educ123.sp.senai.br>
2026-03-27 10:53:11 -0300

    adições php

--- data.php
+++ data.php
@@ -7,4 +7,45 @@ $navProducts = "Produtos";
 $navZenit = "Zenit";
 $navContact = "Contato";
 
+$categorias = [
+    'cFio' => 'Com Fio',
+    'sFio' => 'Sem Fio'
+];
+
+$produtos_base = [
+    [
+        'id' => 1,
+        'nome' => 'Teclado Mecânico - Zenit',
+        'preco' => 259.99,
+        'categoria' => 'sFio',
+        'descricao_curta' => 'Teclado da mecânico oficial da Zenit',
+        'descricao' => 'O Teclado utiliza switches mecânicos (Vermelho, Azul, Marrom), podendo se adequar ao padrão de qualquer Gamer! É produzido com material de mais alta qualidade. <br>
+        Esse Teclado pode também ser customizado de acordo com o gosto do comprador, basta entrar em contato com a nossa equipe ao realizar a compra.',
+        'imagem' => './imagens/imagensProdutos/tecladoZenit.png'
+        //'imagem2' => './imagens/imagensProdutos/tecladoBarebone'
+    ],
+    [
+        'id' => 2,
+        'nome' => 'Mouse Gamer - Zenit',
+        'preco' => 159.99,
+        'categoria' => 'cFio',
+        'descricao_curta' => 'Mouse Gamer oficial da Zenit',
+        'descricao' => 'O Mouse Gamer da Zenit tem RGB customizável por driver e botões laterais, podendo se adequar ao padrão de qualquer Gamer! É produzido com material de mais alta qualidade. <br>
+        Esse Mouse pode também ser customizado de acordo com o gosto do comprador, basta entrar em contato com a nossa equipe ao realizar a compra.',
+        'imagem' => './imagens/imagensProdutos/mouseZenit.png'
+        //'imagem2' => './imagens/imagensProdutos/mouseZenit2'
+    ],
+    [
+        'id' => 3,
+        'nome' => 'Lightbar - Zenit',
+        'preco' => 49.99,
+        'categoria' => 'cFio',
+        'descricao_curta' => 'Lightbar oficial da Zenit',
+        'descricao' => 'A Lightbar da Zenit tem cor de LED customizável por driver, podendo se adequar ao padrão de qualquer Gamer! É produzido com material de mais alta qualidade. <br>
+        Essa Lightbar pode também ser customizado de acordo com o gosto do comprador, basta entrar em contato com a nossa equipe ao realizar a compra.',
+        'imagem' => './imagens/imagensProdutos/lightbarZenit.png'
+        //'imagem2' => './imagens/imagensProdutos/lightbarZenit2'
+    ]
+];
+
 ?>
--- productsPage.php
+++ productsPage.php
@@ -1,5 +1,9 @@
 <?php
-include 'data.php'
+include 'data.php';
+// print '<pre>';
+// print_r($categorias); // print_r Imprime um array
+// print_r($produtos_base);
+// print '</pre>'
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -18,27 +22,57 @@ include 'data.php'
             <div class="sectionTitle">
                 <h2>Produtos</h2>
             </div>
+            <!-- <nav class="navbar" aria-label="Categorias">
+                <ul>
+                    <li><a href="#cat-todos">Todos</a></li>
+                    <li><a href="#cat-cFio">Com Fio</a></li>
+                    <li><a href="#cat-sFio">Sem Fio</a></li>
+                </ul> 
+            </nav> -->
+            <?php
+                echo '<nav class="navbar" aria-label="Categorias">';
+                echo '<ul>';
+                echo '<li><a href="#cat-todos">Todos</a></li>';
+                foreach($categorias as $kcat => $nome){
+                    // kcat = key categoria | vcat = value categoria (ou nome)
+                    echo '<li><a href="#cat-'.$kcat.'">'.$nome.'</a></li>';
+                }
+                echo '</ul>';
+                echo '</nav>';
+            ?>
             <!--organizei os produtos em classes específicas e utilizei variáveis no CSS ( "--nome:valor" e chamando com "var(--nome)" ) para definir as imagens de fundo em cada .productsBox, aplicando uma regra de hover no contêiner pai para disparar simultaneamente a troca de posição, a sombra e a troca pra a segunda imagem.-->
             <!-- quando cria/chama a classe com espaço no nome, são criadas/chamadas duas classes -->
             <div class="productsContainer">
-                <a href="./descricaoProduto.php" class="boxXdesc tecladoZenit"> <!--alterei pra tag <a> pra que seja clicavel e chame a pagina de compra-->
+                <?php
+                foreach($produtos_base as $produto){
+                    echo 
+                    '<a href="./descricaoProduto.php" class="boxXdesc tecladoZenit"> <!--alterei pra tag <a> pra que seja clicavel e chame a pagina de compra-->
                     <div class="productsBox"></div>
                     <div class="productsDescription">
-                        <p>Teclado Mecânico - Zenit | R$15.99</p>
+                        <p>'.$produto['nome'].' | R$'.$produto['preco'].'</p>
+                        <p class="card_categoria">'.$produto['categoria'].'</p>
+                    </div>
+                    </a>';
+                }
+                ?>
+                <!-- <a href="./descricaoProduto.php" class="boxXdesc tecladoZenit"> 
+                    <div class="productsBox"></div>
+                    <div class="productsDescription">
+                        <p>Teclado Mecânico - Zenit | R$259.99</p>
                     </div>
                 </a>
                 <a href="./descricaoProduto.html" class="boxXdesc mouseZenit">
                     <div class="productsBox"></div>
                     <div class="productsDescription">
-                        <p>Mouse Gamer - Zenit | R$15.99</p>
+                        <p>Mouse Gamer - Zenit | R$159.99</p>
                     </div>
                 </a>
                 <a href="./descricaoProduto.php" class="boxXdesc lightbarZenit">
                     <div class="productsBox"></div>
                     <div class="productsDescription">
-                        <p>Lightbar - Zenit | R$15.99</p>
+                        <p>Lightbar - Zenit | R$49.99</p>
                     </div>
-                </a>
+                </a> -->
             </div>
             <div class="productsContainer">
                 <a href="./descricaoProduto.php" class="boxXdesc headsetZenit">
@@ -53,7 +87,7 @@ include 'data.php'
                         <p>Hub USB 10-em-1 - Zenit | R$15.99</p>
                     </div>
                 </a>
-                <a href="./descricaoProduto.html" class="boxXdesc mousepadZenit">
+                <a href="./descricaoProduto.php" class="boxXdesc mousepadZenit">
                     <div class="productsBox"></div>
                     <div class="productsDescription">
                         <p>Mousepad - Zenit | R$15.99</p>
